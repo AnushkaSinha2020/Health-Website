599e488b519bbaee486769379c44c3060a63adec
AnushkaSinha2020 <75123533+AnushkaSinha2020@users.noreply.github.com>
2021-05-22 18:12:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function cough(){
+    window.location="cough.html";
+}
+
+function cold(){
+    window.location="cold.html";
+}
+
+function fever(){
+    window.location="fever.html";
+}
+
+function malaria(){
+    window.location="malaria.html";
+}
+
+function dengue(){
+    window.location="dengue.html";
+}
+
+function diarrhea(){
+    window.location="diarrhea.html";
+}
+
+function covid(){
+    window.location="covid-19.html";
+}
